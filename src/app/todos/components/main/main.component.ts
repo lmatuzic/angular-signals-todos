dea2b7799a1f53ab7a99d5d0a241bffe63372610
Luka Matuzic <luka.matuzic@leapwise.co>
2024-08-29 10:54:14 +0200

    separate todos somponents to their own folders and files

---
+++ src/app/todos/components/main/main.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-todos-main',
+  templateUrl: './main.component.html',
+  standalone: true,
+})
+export class MainComponent {}
